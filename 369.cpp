cfc74efa6ddefc15d2ea37c4d19cae6d76d1e726
Noelia <noelia.herranz@hotmail.es>
2017-11-21 11:55:14 +0100

    Reto 369 en C++

---
+++ 369.cpp
@@ -0,0 +1,24 @@
+//Contando en la arena :https://www.aceptaelreto.com/problem/statement.php?id=369&cat=102
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int num;
+
+	cin >> num;
+
+	while (num != 0) {
+		for (int i = 0; i < num; i++) {
+			cout << 1;
+		}
+		cout << endl;
+
+		cin >> num;
+	}
+
+	return 0;
+
+}
